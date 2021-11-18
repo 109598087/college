65c5f04f8227e32590b74f2853b130d7fb767477
109598087 <t109598087@ntut.org.tw>
2021-11-18 14:22:42 +0800

    value

--- get_111_year.py
+++ get_111_year.py
@@ -7,15 +7,16 @@ driver.get(url_111)
 driver.implicitly_wait(3)
 driver.maximize_window()
 
-
 # get discipline_cluster_list
-driver.find_element(By.XPATH, "//*[contains(text(), '依學群分類查詢')]").click()
-option_list = driver.find_elements(By.XPATH, "//*[@id='LPM_sel_gsdgroup']//option")
-
-discipline_cluster_list = list()
-for option in option_list:
-    if '學群' in option.text and '其他' not in option.text:
-        discipline_cluster_list.append(option.text)
-print(discipline_cluster_list)
+driver.find_element(By.XPATH, "//*[contains(text(), '學群')]").click()
+option_element_list = driver.find_elements(By.XPATH, "//*[contains(text(), '學群')]")
 
 
+discipline_cluster_value_list = list()
+for option_element in option_element_list:
+    if option_element.get_attribute('value') is None:
+        continue
+    if option_element.get_attribute('value').isnumeric():
+        if int(option_element.get_attribute('value')) > 0:
+            discipline_cluster_value_list.append(option_element.get_attribute('value'))
+print(discipline_cluster_value_list)

c26eb1cfe7d92b53ebca5edd2129a27c3f23c5c7
t109598087@ntut.org.tw <t109598087@ntut.org.tw>
2021-11-18 21:19:28 +0800

    輕輕鬆鬆到最裡面

--- get_111_year.py
+++ get_111_year.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
@@ -17,17 +19,17 @@ def get_option_value_list(option_element_list):
 driver = webdriver.Chrome('chromedriver.exe')
 url_111 = 'https://www.cac.edu.tw/cacportal/jbcrc/LearningPortfolios_MultiQuery_ppa/index.php'
 driver.get(url_111)
-driver.implicitly_wait(3)
+driver.implicitly_wait(5)
 driver.maximize_window()
 
 # get discipline_cluster_list
-driver.find_element(By.XPATH,
-                    "//*[contains(@class, 'mb-0')]//*[contains(@class, 'btn btn-link') and contains(text(), '學群')]").click()
+driver.find_element(By.XPATH, "//*[contains(text(), '依學群分類查詢')]").click()
 option_element_list1 = driver.find_elements(By.XPATH, "//*[contains(text(), '學群')]")
 discipline_cluster_value_list = get_option_value_list(option_element_list1)
 print(discipline_cluster_value_list)
 Select(driver.find_element(By.XPATH, "//*[contains(@id, 'LPM_sel_gsdgroup')]")).select_by_value("1")
 
+time.sleep(1)
 driver.find_element(By.XPATH, "//*[contains(@id, 'SelGsdName')]").click()
 option_element_list2 = driver.find_elements(By.XPATH, "//*[contains(@id, 'SelGsdName')]//option[contains(@value, '')]")
 print(len(option_element_list2))
@@ -38,6 +40,19 @@ Select(driver.find_element(By.XPATH, "//*[contains(@id, 'SelGsdName')]")).select
 driver.find_element(By.XPATH,
                     "//*[contains(@id, 'LPM_form_gsdgroup')]//*[contains(@class, 'btn') and text()='查詢']").click()
 
+driver.switch_to.window(driver.window_handles[1])
 
-# # get 學程
-# SelGsdName
+# get school
+school_element_list = driver.find_elements(By.XPATH,
+                                           "//*[contains(@class, 'btn btn-outline-secondary btn-light')]")
+school_list = [school_element.text for school_element in school_element_list]
+
+driver.find_element(By.XPATH, "//div[contains(text(), " + "\"" + school_list[0].split('-')[1] + "\"" + ")]").click()
+
+driver.find_element(By.XPATH,
+                    "//*[contains(@class, 'LPM_colgsd_txt') and contains(text(), " + "\"" + school_list[0].split('-')[
+                        1] + "\"" + ")]//parent::a").click()
+driver.switch_to.window(driver.window_handles[2])
+
+
+# 抓文字

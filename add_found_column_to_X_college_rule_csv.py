31c663af7529a3b31f9bfc589bc30bd7df8e450c
pc092 <t109598092@ntut.org.tw>
2022-01-16 14:08:09 +0800

    [fix]修好111年科系申請入學簡章重點抓取(1學群->1csv->18csv)會每次一直累加的問題,故不需要使用delete_duplicated_college_rule 並重新執行程式更新csv

--- add_found_column_to_X_college_rule_csv.py
+++ add_found_column_to_X_college_rule_csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 key_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R']
 for discipline in key_list:
-    df = pd.read_csv('delete_duplicated_college_rule_file/' + discipline + '_college_rule.csv')
+    df = pd.read_csv('college_rule_18_file/' + discipline + '_college_rule.csv')
     print(df)
     df['found'] = [0 for i in range(len(df['college_name']))]
     df.to_csv('add_found_column_to_college_rule_file/' + discipline + '_college_rule.csv', index=False)
